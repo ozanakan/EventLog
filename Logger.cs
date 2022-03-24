142f8c3dd4b6d7a1a737a1b5fe04158059b5f2b2
Ozan Akan <73120826+ozanakan@users.noreply.github.com>
2022-03-24 17:18:50 +0300

    EventLog

---
+++ Logger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleEventLog
+{
+    class Logger
+    {
+        public void Log(Exception e)
+        {
+            var exception = e as SqlException;
+            SqlError(e.Message, exception.Errors);
+        }
+        private static void AddLog(string logMessage)
+        {
+            if (!EventLog.SourceExists("Sample"))
+            {
+                EventLog.CreateEventSource("Sample", "Sample");
+            }
+            var log = new EventLog();
+            log.Source = "Sample";
+            log.WriteEntry(logMessage, EventLogEntryType.Error);
+        }
+
+        private static void SqlError(string logMessage, SqlErrorCollection sqlErrors)
+        {
+            var sb = new StringBuilder();
+            foreach (SqlError item in sqlErrors)
+            {
+                sb.AppendLine("Hata Tarihi: ");
+                sb.Append(DateTime.Now);
+                sb.Append(Environment.NewLine);
+                sb.AppendLine("Satır Numarası: ");
+                sb.Append(item.LineNumber);
+                sb.Append(Environment.NewLine);
+                sb.AppendLine("Mesaj: ");
+                sb.Append(item.Message);
+
+                AddLog(sb.ToString());
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ConsoleEventLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Connection();
+            Console.ReadKey();
+
+        }
+
+
+        private static void Connection()
+        {
+          var  con = new SqlConnection("Server=DESKTOP-PU7I8L5;database=northwnd;trusted_connection=true");
+          try
+          {
+              var cmd = new SqlCommand();
+              con.Open();
+              cmd.Connection = con;
+              cmd.CommandText = "Select count(*) from Orders";
+              var count = (int)cmd.ExecuteScalar();
+
+              Console.WriteLine(count);
+          }
+          catch (Exception e)
+          {
+              var logger = new Logger();
+              logger.Log(e);
+          }
+          finally
+          {
+              if (con.State == ConnectionState.Open)
+                con.Close();
+          }
+
+
+        }
+
+    }
+}
